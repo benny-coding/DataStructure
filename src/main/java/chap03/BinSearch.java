ef67b5e5dc384e639d2e141d4fd5b25e139297fe
ohdongpodong <ohdongpodong@gmail.com>
2019-09-06 23:43:25 +0900

    DataStructure10

--- src/main/java/chap03/BinSearch.java
+++ src/main/java/chap03/BinSearch.java
@@ -1,16 +1,17 @@
 package chap03;
 import java.util.Scanner;
+// 이진 검색
 
-public class BinSearch {
-    // 요솟수가  n인 배열 a에서 key와 같은 요소를 이진 검색합니다.
-    static int binSearch(int[] a, int n, int key){
-        int pl = 0;     // 검색 범위의 첫 인덱스
-        int pr = n - 1; // 검색 범위의 끝 인덱스
+class BinSearch {
+    // 요솟수가 n인 배열 a에서 key와 같은 요소를 이진 검색합니다.
+    static int binSearch(int a[], int n, int key){
+        int pl = 0;         // pl은 처음 인덱스를 나타내며
+        int pr = n -1;      // pr은 마지막 인덱스를 나타낸다.
 
         do {
-            int pc = (pl + pr) / 2;
-            if (a[pc] == key){
-                return pc;
+            int pc = (pl + pr) / 2; // 중앙 요소의 인덱스
+            if(a[pc] == key){
+                return pc;      // 검색 성공!
             } else if(a[pc] < key){
                 pl = pc + 1;
             } else {
@@ -26,30 +27,28 @@ public class BinSearch {
 
         System.out.print("요솟수 : ");
         int num = stdIn.nextInt( );
-        int[] x = new int[num];         // 요솟수가 num인 배열
+        int[] x = new int[num];      // 요솟수가 num인 배열
 
-        System.out.println("오름차순으로 입력하세요.");
+        System.out.println("오름차순으로 입력하세요");
 
-        System.out.print("x[0]: ");         // 첫 요소 입력
-        x[0] = stdIn.nextInt( );
+        System.out.print("x[0] : ");    // 첫 요소 입력
+        x[0] = stdIn.nextInt();
 
-        for (int i = 1; i < num ; i++) {
+        for(int i = 1; i < num; i++){
             do {
-                System.out.print("x[" + i + "]: ");
+                System.out.print("x[" + i + "] : ");
                 x[i] = stdIn.nextInt();
-            } while (x[i] < x[i-1]);        // 바로 앞의 요소보다 작으면 다시 입력
+            } while (x[i] < x[i - 1]);      // 바로 앞의 요소보다 작으면 다시 입력
         }
+        System.out.print("검색할 값 : ");   // 키 값을 입력
+        int searchKey = stdIn.nextInt();
 
-        System.out.print("검색할 값 :");        // 키 값을 입력
-        int ky = stdIn.nextInt();
-
-        int idx = binSearch(x, num, ky);    // 배열 x에서 키 값이 ky인 요소를 검색
+        int idx = binSearch(x, num, searchKey); // 배열 x에서 키 값이 searchKey인 요소를 검색
 
         if(idx == -1){
             System.out.println("그 값의 요소가 없습니다.");
         } else {
-            System.out.println(ky + "은(는) x[" + idx + "]에 있습니다.");
+            System.out.println(searchKey + "은(는) x[" + idx + "]에 있습니다.");
         }
     }
-
 }
