8c737883731f69016804beae6d05ab6024d08d8e
ohdongpodong <ohdongpodong@gmail.com>
2019-10-01 23:41:08 +0900

    DataStructure31

--- src/main/java/lifeCoding/doublyLinkedList/DoublyLinkedList.java
+++ src/main/java/lifeCoding/doublyLinkedList/DoublyLinkedList.java
@@ -90,6 +90,9 @@ public class DoublyLinkedList {
         head = head.next;
         Object returnData = temp.data;
         temp = null;
+        if(head != null){
+            head.prev = null;
+        }
         size--;
         return returnData;
     }
@@ -101,6 +104,9 @@ public class DoublyLinkedList {
         Node temp = node(k-1);
         Node tododeleted = temp.next;
         temp.next = temp.next.next; // # 해당 코드는 NullPointerexception 발생함 예외처리가 필요
+        if(temp.next != null){
+            temp.next.prev = temp;
+        }
         Object returnData = tododeleted.data;
         if(tododeleted == tail){
             tail = temp;
@@ -168,12 +174,27 @@ public class DoublyLinkedList {
 
         ListIterator(){
             next = head;
+            nextIndex = 0;
         }
 
         public Object next(){
             lastReturned = next;
             next = next.next;
             nextIndex++;
+            return lastReturned.data;
+        }
+
+        public boolean hasPrevious(){
+            return nextIndex < size();
+        }
+
+        public Object previous(){
+            if(next == null){
+                lastReturned = next = tail;
+            } else {
+                lastReturned = next = next.prev;
+            }
+            nextIndex--;
             return lastReturned.data;
         }
 
@@ -185,8 +206,16 @@ public class DoublyLinkedList {
                 newNode.next = next;	// 새로운 노드의 next는 next 노드를 가리킵니다.
             } else {
                 lastReturned.next = newNode;	// 새로운 노드를 lastReturned의 next로 연결시키고
-                newNode.next = next;	// next는 새로운 노드의 next로 연결시킵니다.
+                newNode.prev = lastReturned;
+                if(next != null) {
+                    newNode.next = next;    // next는 새로운 노드의 next로 연결시킵니다.
+                    next.prev = newNode;
+                } else {
+                    tail = newNode;
+                }
+
             }
+
             lastReturned = newNode;	// 새로운 노드를 리턴할 노드로 정한 후
             nextIndex++;	// next의 index 증가와
             size++;	// size도 증가시킵니다.
@@ -196,8 +225,37 @@ public class DoublyLinkedList {
             if(nextIndex == 0){
                 throw new IllegalStateException();
             }
-            DoublyLinkedList.this.remove(nextIndex-1);
+            Node n = lastReturned.next;
+            Node p = lastReturned.prev;
+
+            if(p==null){
+                head = n;
+                head.prev = null;
+                lastReturned = null;
+            } else {
+                p.next = next;
+                lastReturned.prev = null;
+            }
+
+            if(n == null){
+                tail = p;
+                tail.next = null;
+            } else {
+                n.prev = p;
+            }
+            if(next == null){
+                lastReturned = tail;
+            } else {
+                lastReturned = next.prev;
+            }
+
+
+
+            size--;
             nextIndex--;
+
+            //DoublyLinkedList.this.remove(nextIndex-1);
+            //nextIndex--;
         }
 
     }
